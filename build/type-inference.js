e065fae924bad9eb688566c13e32cbe2d3a9a512
justin <978336184@qq.com>
2020-05-15 11:48:31 +0800

    master

---
+++ build/type-inference.js
@@ -0,0 +1,14 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var myFavoriteNumber, myFavoriteNumber1;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            myFavoriteNumber = 'seven';
+            myFavoriteNumber1 = 'seven';
+            myFavoriteNumber1 = 7;
+            exports_1("default", { myFavoriteNumber: myFavoriteNumber });
+        }
+    };
+});
---
+++ build/union-type.js
@@ -0,0 +1,12 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var myFavoriteNumber;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            myFavoriteNumber = 'seven';
+            myFavoriteNumber = 7;
+        }
+    };
+});
